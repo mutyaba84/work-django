cc8e9620c3bbd95a9cae412109473590cb2e70d6
mutyaba84 <mutyabadennie@gmail.com>
2024-01-30 19:28:06 +0000

    updates

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'test_app',
+    'test_project',
     
 ]
 
@@ -123,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PIPENV_IGNORE_VIRTUALENVS=1
